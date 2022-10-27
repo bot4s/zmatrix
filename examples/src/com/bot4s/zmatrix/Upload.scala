a8a720d74f0378828c9619e9c6954b620bd3d049
ex0ns <g.tournigand@gmail.com>
2022-10-27 11:54:28 +0200

    local and remote upload example

--- examples/src/com/bot4s/zmatrix/Upload.scala
+++ examples/src/com/bot4s/zmatrix/Upload.scala
@@ -6,18 +6,25 @@ import zio.ZIO
 import com.bot4s.zmatrix.api.media
 import sttp.client3._
 import sttp.model.MediaType
+import java.io.File
 
 object Upload extends ExampleApp[Unit] {
 
-  val upload = media
+  val remoteUpload = media
     .upload(
       uri"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Cc.logo.circle.svg/1024px-Cc.logo.circle.png",
       MediaType.ImagePng
     )
 
+  val localUpload = media
+    .upload(
+      new File("/tmp/194031-2.jpg"),
+      None
+    )
+
   val runExample =
     (for {
-      up     <- upload
+      up     <- remoteUpload
       config <- ZIO.service[MatrixConfiguration].flatMap(_.get)
       _ <- printLine(
              s"Media uploaded, check out ${config.matrix.mediaApi}/download/${up.serverName}/${up.mediaId}"
