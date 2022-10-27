6fbd4b4c03e5a7bf41b615b1f9436c5509e8a3e9
ex0ns <g.tournigand@gmail.com>
2022-10-27 12:14:06 +0200

    Simplify SimpleSync example

--- examples/src/com/bot4s/zmatrix/SimpleSync.scala
+++ examples/src/com/bot4s/zmatrix/SimpleSync.scala
@@ -12,29 +12,25 @@ object SimpleSync extends ExampleApp[Long] {
 
   override def runExample: ZIO[AuthMatrixEnv, MatrixError, Long] = {
     val method: ZIO[AuthMatrixEnv, MatrixError, SyncState] =
-      sync.sync.tapInviteEvent {
-        case (roomId, invite: InviteMemberEvent) if invite.content.membership == "invite" =>
-          ZIO.logInfo(f"Joining $roomId") *> roomMembership.join(roomId)
-      }.tapRoomEvent {
-        case (roomId, TopicEvent(sender, _, TopicMessageContent(topic))) =>
-          rooms.sendMsg(roomId, s"Thanks $sender for setting the topic to '${topic}'")
+      for {
+        config <- MatrixConfiguration.get
+        fromBot = (sender: String) => config.matrix.userId.exists(name => sender.startsWith(s"@$name"))
+        nextState <- sync.sync.tapInviteEvent {
+                       case (roomId, invite: InviteMemberEvent) if invite.content.membership == "invite" =>
+                         ZIO.logInfo(f"Joining $roomId") *> roomMembership.join(roomId)
+                     }.tapRoomEvent {
+                       case (roomId, TopicEvent(sender, _, TopicMessageContent(topic))) =>
+                         rooms.sendMsg(roomId, s"Thanks $sender for setting the topic to '${topic}'")
 
-        case (roomId, MessageEvent(sender, _, content: RoomMessageTextContent)) =>
-          val notFromBot =
-            MatrixConfiguration.get.map(!_.matrix.userId.exists(botName => sender.startsWith(s"@$botName")))
-          ZIO.whenZIO(notFromBot)(
-            ZIO.logInfo(f"${roomId} Message received: ${content.body}") *>
-              rooms.sendMsg(roomId, "welcome back")
-          )
+                       case (roomId, MessageEvent(sender, _, content: RoomMessageTextContent)) if !fromBot(sender) =>
+                         ZIO.logInfo(f"${roomId} Message received: ${content.body}") *>
+                           rooms.sendMsg(roomId, "welcome back")
 
-        case (roomId, MessageEvent(sender, _, content: RoomMessageImageContent)) =>
-          val notFromBot =
-            MatrixConfiguration.get.map(!_.matrix.userId.exists(botName => sender.startsWith(s"@$botName")))
-          ZIO.whenZIO(notFromBot)(
-            ZIO.logInfo(f"${roomId} Image received: ${content.body}") *>
-              rooms.sendEvent(roomId, RoomMessageTextContent("welcome back (image)"))
-          )
-      }.updateState()
+                       case (roomId, MessageEvent(sender, _, content: RoomMessageImageContent)) if !fromBot(sender) =>
+                         ZIO.logInfo(f"${roomId} Image received: ${content.body}") *>
+                           rooms.sendEvent(roomId, RoomMessageTextContent("welcome back (image)"))
+                     }.updateState()
+      } yield nextState
 
     method
       .repeat(Schedule.spaced(10.seconds))
